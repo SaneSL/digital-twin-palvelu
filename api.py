b3364ffc925252a9d1c3c0de6853febd978c3937
Santeri Leppänen <sanesl123@gmail.com>
2020-03-10 23:35:22 +0200

    added api file and some init

---
+++ api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init db
+db = SQLAlchemy(app)
+
+# Init marsmallow
+ma = Marshmallow(app)
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg': 'Moro'})
+
+
+# Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
