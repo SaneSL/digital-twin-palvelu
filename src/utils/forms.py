a7df4534a5a9e8434922d66a4090d856d64770c6
Santeri Leppänen <sanesl123@gmail.com>
2020-04-13 00:03:21 +0300

    Add error printing

--- src/utils/forms.py
+++ src/utils/forms.py
@@ -3,15 +3,15 @@ from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import DataRequired, Email, InputRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
 
 
 class RegisterForm(FlaskForm):
-    company_name = StringField('Company name', validators=[DataRequired()])
-    email = StringField('Email', validators=[Email(), DataRequired()])
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    sub = SelectField('Subscription', choices=[('1', '1 Month'), ('1', '3 Months'), ('1', '6 Months'), ('12', '12 Months')])
+    company_name = StringField('Company name', validators=[InputRequired()])
+    email = StringField('Email', validators=[Email(), InputRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    sub = SelectField('Subscription', choices=[('1', '1 Month'), ('3', '3 Months'), ('6', '6 Months'), ('12', '12 Months')])
     submit = SubmitField('Register')
